667ede6699d4ccfbc44afc0669fc652270d0f540
Nikhil.Marathe <Nikhil.Marathe@WORLSTATION-532>
2022-09-24 19:45:32 +0530

    commit changes for presentation employee Master Controller

---
+++ LALHWebApp/Controllers/MasterEmployeeController.cs
@@ -0,0 +1,23 @@
+﻿using LHLAApplication.Interfaces;
+using LHLAApplication.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LALHWebApp.Controllers
+{
+    public class MasterEmployeeController : Controller
+    {
+        private IEmployeeService _employeeService;
+
+        public MasterEmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        public IActionResult Index()
+        {
+            VMEmployee employee = _employeeService.GetMasterEmployees();
+
+            return View(employee);
+        }
+    }
+}
